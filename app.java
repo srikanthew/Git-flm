ce33f3d7abe9a0d51848272d6eacaf97930a8d18
srikanthew <166531426+srikanthew@users.noreply.github.com>
2026-04-07 21:30:24 +0530

    swap number

--- app.java
+++ app.java
@@ -1,5 +1,13 @@
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class GFG{
+    public static void main(String[] args){
+        
+        int m = 9, n = 5;
+        System.out.println("Before swapping: m = " + m + ", n = " + n);
+
+        int temp = m;
+        m = n;
+        n = temp;
+
+        System.out.println("After swapping: m = " + m + ", n = " + n);
     }
 }
